aff60691fffedf652b8a0f060265cb048345763c
Dan Ficklin <danficklin@fakemail.com>
2022-01-19 14:00:39 -0500

    Added framework for library, tests, and UI

---
+++ Cafe/MenuTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MenuTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Where we will test our repository
+
+        }
+    }
+}
---
+++ Cafe/MenuUI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MenuUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("External console is working. Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
